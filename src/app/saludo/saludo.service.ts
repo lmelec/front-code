6d198d1529a7a95e31e8732fd8244066205eef1a
Luis Melendez <melendezcampis@gmail.com>
2019-10-08 12:28:23 -0500

    6

--- src/app/saludo/saludo.service.ts
+++ src/app/saludo/saludo.service.ts
@@ -7,7 +7,8 @@ import { Persona } from './persona';
   providedIn: 'root'
 })
 export class SaludoService {
-  url = 'http://dockerserver.devopsmind.co:8888/saludo/';
+  #url = 'http://dockerserver.devopsmind.co:8888/saludo/';
+  url = 'http://devopsmind.co/api';
   constructor(private http: HttpClient) { }
 
   saludar(): Observable<Persona>{
